56dd54de86d518339f84378936f6bc04fcaed832
SUJI-J <suzy852@naver.com>
2023-11-20 14:10:30 +0900

    2023.11.19

--- Baek_11651.java
+++ Baek_11651.java
@@ -1,5 +1,6 @@
 package BaekJoon;
 //11651
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -11,21 +12,21 @@ public class Baek_11651 {
         int cnt = Integer.parseInt(br.readLine());
         int[][] arr = new int[cnt][2];
 
-        for (int i=0;i<cnt;i++){
+        for (int i = 0; i < cnt; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (o1, o2)->{
-            if(o1[1] == o2[1]){
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
                 return Integer.compare(o1[0], o2[0]);
-            } else{
+            } else {
                 return Integer.compare(o1[1], o2[1]);
             }
         });
 
-        for(int i=0;i<cnt;i++){
+        for (int i = 0; i < cnt; i++) {
             bw.write(arr[i][0] + " " + arr[i][1] + "\n");
         }
 
