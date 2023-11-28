df469188aaaed5c71e702ebd2affb632d73799bb
SUJI-J <suzy852@naver.com>
2023-11-28 14:02:17 +0900

    2023.11.28

---
+++ bronze/Baek_1085.java
@@ -0,0 +1,23 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(x, y);
+        min = Math.min(min, w - x);
+        min = Math.min(min, h - y);
+
+        System.out.println(min);
+    }
+}
