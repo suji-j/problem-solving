69329ac1196411a9a20d9301b181766c9feedfd2
SUJI-J <suzy852@naver.com>
2023-12-05 13:26:02 +0900

    2023.12.05

---
+++ bronze/Baek_2903.java
@@ -0,0 +1,13 @@
+package BaekJoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_2903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int) Math.pow(Math.pow(2, N) + 1, 2));
+    }
+}
