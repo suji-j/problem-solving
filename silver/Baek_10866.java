c431cea75f0c61238abbfe64389dd0dfeacac213
SUJI-J <suzy852@naver.com>
2023-12-19 13:46:57 +0900

    2023.12.19

---
+++ silver/Baek_10866.java
@@ -0,0 +1,69 @@
+package BaekJoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Baek_10866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    deque.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.removeFirst()).append("\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.removeLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    if (deque.isEmpty()) {
+                        sb.append("1\n");
+                    } else {
+                        sb.append("0\n");
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.getFirst()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.getLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
