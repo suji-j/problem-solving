fde762acb4700cd0a93188e1563326c512d5f121
SUJI-J <suzy852@naver.com>
2024-01-28 16:47:04 +0900

    2024.01.28

---
+++ silver/Baek_1927.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            int order = Integer.parseInt(br.readLine());
+            if (order == 0) {
+                if (priorityQueue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(priorityQueue.poll());
+                }
+            } else {
+                priorityQueue.add(order);
+            }
+        }
+    }
+}
