4d4a309de5abc8ca8bc6b08d4c2725404dc151f7
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2024-05-28 14:42:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,36 +2,31 @@ import java.util.StringTokenizer;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        
-        int backcnt = 0;
-        for (int i = s.length()-1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') backcnt += 1;
-            else break;
-        }
-        
+        int backCnt = 0;
         String[] arr = s.split(" ");
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') backCnt += 1;
+            else break;
         }
         
         for (int i = 0; i < arr.length; i++) {
             String str = arr[i];
+            
             if (str.isBlank()) {
                 sb.append(" ");
             } else {
                 sb.append(str.substring(0, 1).toUpperCase()).append(str.substring(1, str.length()).toLowerCase());
-                if (i != arr.length-1) sb.append(" ");
+                if (i != arr.length - 1) sb.append(" ");
                 else { 
-                    for (int j = 0; j < backcnt; j++) {
+                    for (int j = 0; j < backCnt; j++) {
                         sb.append(" ");
                     }
                 }
             }
         }
         
-        answer = sb.toString();
-        return answer;
+        return sb.toString();
     }
 }
