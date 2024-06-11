3394c56038e59b4451aedb4b64fc4f994a0eca09
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2024-06-11 19:18:21 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,7 +7,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N][3];
-        int[][] house = new int[N][3];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; i++) {
@@ -17,15 +16,13 @@ public class Main {
 
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                house[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + house[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + house[i][2];
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + r;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + g;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + b;
         }
 
         System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
