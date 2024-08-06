56a5bb91231a8b0cc19c65a0f685e7d755521ceb
Suji Jang <80563308+SUJI-J@users.noreply.github.com>
2024-08-06 17:10:21 +0900

    [level 2] Title: 올바른 괄호, Time: 15.34 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,8 +1,7 @@
 import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        
         Stack<Character> stack = new Stack<>();
         char[] arr = s.toCharArray();
 
@@ -10,7 +9,9 @@ class Solution {
             if (c == '(') {
                 stack.add('(');
             } else {
-                if (stack.isEmpty()) return false;
+                if (stack.isEmpty()) {
+                    return false;
+                }
                 stack.pop();
             }
         }
