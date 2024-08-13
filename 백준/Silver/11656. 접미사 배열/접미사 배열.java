23932c2d1ac21ad0b497b6d2cc1a6a9847dd5d07
Suji Jang <80563308+SUJI-J@users.noreply.github.com>
2024-08-13 09:18:47 +0900

    [Silver IV] Title: 접미사 배열, Time: 216 ms, Memory: 16152 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.substring(i));
+        }
+        Collections.sort(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
