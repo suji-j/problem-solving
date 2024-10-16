c54cb7abb9525fa527d4d2ce9ffbda9038cc10e0
Suji Jang <80563308+SUJI-J@users.noreply.github.com>
2024-10-16 16:02:52 +0900

    [level 0] Title: 문자열 출력하기, Time: 176.76 ms, Memory: 59.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException; 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        System.out.print(a);
+    }
+}
