51c49d81af83babc6360299ff9e2f2f84ec1fa51
Suji Jang <80563308+SUJI-J@users.noreply.github.com>
2024-10-16 16:11:01 +0900

    [level 0] Title: 수 조작하기 1, Time: 9.67 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] arr = control.toCharArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'w') {
+                n += 1;
+            } else if (arr[i] == 's') {
+                n -= 1;
+            } else if (arr[i] == 'd') {
+                n += 10;
+            } else {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
