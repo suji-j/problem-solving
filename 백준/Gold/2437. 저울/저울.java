95a14d447ca07ee21d03d077aa8d794e69500644
Suji Jang <ssu2dev@gmail.com>
2025-02-05 15:49:13 +0900

    [Gold II] Title: 저울, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int minSum = 0;
+        for (int i : arr) {
+            if (minSum + 1 < i) {
+                break;
+            }
+            minSum += i;
+        }
+
+        System.out.println(minSum + 1);
+    }
+}
