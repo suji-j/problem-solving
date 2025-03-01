3378b665a32739169e6d6ba1b383f9a8554a69a5
Suji Jang <ssu2dev@gmail.com>
2025-03-01 12:29:50 +0900

    [Silver III] Title: N과 M (5), Time: 424 ms, Memory: 44460 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] num;
+    static List<Integer> result = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        back(0, 0);
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    static void back(int start, int depth) {
+        if (depth == m) {
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!result.contains(num[i])) {
+                result.add(num[i]);
+                back(start + 1, depth + 1);
+                result.remove(result.size() - 1);
+            }
+        }
+    }
+}
