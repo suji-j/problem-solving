dfdfea69e76c0a4bc94bf37ac2a8075a68da2368
Suji Jang <ssu2dev@gmail.com>
2025-03-14 12:43:54 +0900

    [Silver III] Title: 합 구하기, Time: 620 ms, Memory: 51540 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] preSum = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            preSum[i] = preSum[i - 1] + arr[i];
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            bw.write(preSum[end] - preSum[start - 1] + "\n");
+        }
+        bw.close();
+    }
+}
