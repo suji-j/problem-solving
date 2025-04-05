55c21ba0a9653b66d26d4026b13ed39dea0edf6d
Suji Jang <ssu2dev@gmail.com>
2025-04-05 12:09:27 +0900

    [Silver II] Title: N과 M (12), Time: 176 ms, Memory: 19372 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static List<Integer> result = new ArrayList<>();
+    static Set<String> ans = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        back(0, 0);
+
+        for (String str : ans) {
+            bw.write(str + "\n");
+        }
+        bw.close();
+    }
+
+    static void back(int start, int depth) {
+        if (depth == m) {
+            StringBuilder sb = new StringBuilder();
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+            ans.add(sb.toString());
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            result.add(arr[i]);
+            back(i, depth + 1);
+            result.remove(result.size() - 1);
+        }
+    }
+}
