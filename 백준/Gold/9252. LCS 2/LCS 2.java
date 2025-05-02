4873a2934efff0f6f33f364630712d0b0eb035c7
Suji Jang <ssu2dev@gmail.com>
2025-05-02 12:56:51 +0900

    [Gold IV] Title: LCS 2, Time: 184 ms, Memory: 21564 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= str1.length(); i++) {
+            char ch1 = str1.charAt(i - 1);
+            for (int j = 1; j <= str2.length(); j++) {
+                char ch2 = str2.charAt(j - 1);
+                if (ch1 == ch2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i = str1.length();
+        int j = str2.length();
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        sb.reverse();
+
+        bw.write(dp[str1.length()][str2.length()] + "\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
