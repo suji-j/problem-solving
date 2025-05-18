fec1440b5ff5e5890bbf47a8b5bebe5e80f98f52
Suji Jang <ssu2dev@gmail.com>
2025-05-18 16:43:18 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 872 ms, Memory: 114796 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int[] sum = new int[n + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + arr[i - 1];
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+            bw.write(sum[r] - sum[l - 1] + "\n");
+        }
+        bw.close();
+    }
+}
