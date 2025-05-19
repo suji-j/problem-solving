8308dad6566551466c5690fccb3fe525828ec0e7
Suji Jang <ssu2dev@gmail.com>
2025-05-19 12:53:17 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 292 ms, Memory: 25852 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long[] preSum = new long[n + 1];
+        preSum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + arr[i - 1];
+        }
+
+        long max = 0;
+        for (int i = m; i <= n; i++) {
+            max = Math.max(max, preSum[i] - preSum[i - m]);
+        }
+
+        bw.write(String.valueOf(max));
+        bw.close();
+    }
+}
