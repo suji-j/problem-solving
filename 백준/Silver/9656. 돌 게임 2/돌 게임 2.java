ab9ca119f799f76e50f730b6b4bf0ae4358f66fa
Suji Jang <ssu2dev@gmail.com>
2025-05-30 11:59:15 +0900

    [Silver V] Title: 돌 게임 2, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(n % 2 == 0 ? "SK" : "CY");
+        bw.close();
+    }
+}
